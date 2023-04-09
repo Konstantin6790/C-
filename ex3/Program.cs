0232ebb3faacb55976275179c629637b3165e03e
Konstantin_Okolokulak <outcold6790@gmail.com>
2023-04-09 13:46:49 +0300

    ex3 найти четное

---
+++ ex3/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("Input number1");
+double number1 = double.Parse(Console.ReadLine());
+
+if (number1 % 2 == 0)
+    Console.WriteLine($"{number1} - чётное");   
+else
+    Console.WriteLine($"{number1} - нечётное");
+
+
+
+
