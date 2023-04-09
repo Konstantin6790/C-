8faad5baa3dc7b0517a370dc4f67fc31e241ff03
Konstantin_Okolokulak <outcold6790@gmail.com>
2023-04-09 16:55:37 +0300

    ДЗ из трехзначного 2-ую цифру

---
+++ lesson2.1/Program.cs
@@ -0,0 +1,14 @@
+﻿// Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+
+Console.Write("Введите трехзначное число:");
+int number = Convert.ToInt32(Console.ReadLine());
+int Length = number.ToString().Length;
+
+if (Length == 3) {
+		int result = (number / 10) % 10;
+		Console.WriteLine($"Вторая цифра это {result}");
+} 
+else 
+{
+		Console.WriteLine("Введено не трехзначное число!!!");
+}
