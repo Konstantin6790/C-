da47c5700a690f5fd7f4cb45da4ee548eefa33da
Konstantin_Okolokulak <outcold6790@gmail.com>
2023-04-09 13:16:10 +0300

    ex1 максимум из 2-х чисел

---
+++ ex1/Program.cs
@@ -0,0 +1,8 @@
+﻿Console.WriteLine("Input number1");
+int number1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Input number2");
+int number2 = Convert.ToInt32(Console.ReadLine());
+int max = number1;
+if (number2 > max) max = number2;
+
+Console.WriteLine($"максимальное значение = {max}");
