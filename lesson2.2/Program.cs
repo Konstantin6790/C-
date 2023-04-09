919d13b6ec8ebe50c276660c52b538b9055c72f0
Konstantin_Okolokulak <outcold6790@gmail.com>
2023-04-09 17:07:13 +0300

    ДЗ вывод 3-ей цифры или вывод что её нет

---
+++ lesson2.2/Program.cs
@@ -0,0 +1,15 @@
+﻿// Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+
+Console.Write("Введите число:");
+int number = Convert.ToInt32(Console.ReadLine());
+int Length = number.ToString().Length;
+if (Length >= 3) {
+	while (number > 999)
+	{
+		number = number / 10;
+	}
+	int result = number % 10;
+	Console.WriteLine("Третья цифра числа " + result);
+} else {
+	Console.WriteLine("В введенном числе нет третьей цифры");
+}
