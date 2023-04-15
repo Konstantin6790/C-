3f6eb69b517241b0ed404c1d655683d349641cbb
Konstantin_Okolokulak <outcold6790@gmail.com>
2023-04-15 23:41:00 +0300

    программа, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N

---
+++ dz3.3/Program.cs
@@ -0,0 +1,11 @@
+﻿//Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+Console.Write("Введите число:");
+int n = int.Parse(Console.ReadLine());
+void Kub(int n)
+{
+  for (int i = 1; i <= n; i++)
+  {
+    System.Console.Write(" " + Math.Pow(i, 3));
+  }
+}
+Kub(n);
