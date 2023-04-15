e7e0a240e9b35af0cebbc33757b363c4f7c7c18f
Konstantin_Okolokulak <outcold6790@gmail.com>
2023-04-15 16:02:20 +0300

    Напишите программу, которая по номеру четверти возвращает какие координаты будут

---
+++ les3.2/Program.cs
@@ -0,0 +1,16 @@
+﻿// По номеру четверти возвращает какие координаты будут
+Console.WriteLine("Укажите четверть:");
+int x = int.Parse (Console.ReadLine());
+
+void Chetvert(int a)
+{
+    switch (a)
+    {
+	case 1: System.Console.WriteLine("x > 0, y > 0"); break;
+    case 2: System.Console.WriteLine("x < 0, y > 0"); break;
+    case 3: System.Console.WriteLine("x < 0, y < 0"); break;
+    case 4: System.Console.WriteLine("x < 0, y > 0"); break;
+    default: System.Console.WriteLine("x = 0 и/или y = 0"); break;
+    }
+}
+Chetvert(x);
