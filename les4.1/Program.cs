bb11520a655bd86c2fcfbdadc2ecf88c1b089ac8
Konstantin_Okolokulak <outcold6790@gmail.com>
2023-04-22 15:02:09 +0300

    создание массива

---
+++ les4.1/Program.cs
@@ -0,0 +1,28 @@
+﻿void FillArray(int[] numbers)
+{
+    Random rnd = new Random();
+    for (int i = 0; i < numbers.Length; i++)
+    {
+    numbers[i] = rnd.Next(1, 10);
+    }
+}
+//распечать массив
+void PrintArray(int[] numbers)
+{
+    for (int i = 0; i < numbers.Length; i++)
+    {
+        Console.Write($"{numbers[i]} ");
+    }
+    Console.WriteLine();
+}
+//Задайте массив из 12 элементов, заполненный случайными числами из промежутка [-9, 9]. Найдите сумму отрицательных и положительных элементов массива.
+void Task0()
+{
+    int size = 10;
+    int [] numbers = new int[size];
+    FillArray(numbers);
+    PrintArray(numbers);
+}
+
+Task0();
+    
