e42b192ef3fdcf3143bffa16263eaa229c7a9cac
Okolokulak_Konstantin <outcold6790@gmail.com>
2023-04-29 14:14:18 +0300

    урок 7.1

---
+++ les7.1/Program.cs
@@ -0,0 +1,33 @@
+﻿int[,] array = new int[4, 4];
+
+FillArray(array);
+PrintArray(array);
+
+int[,] CreateTwoDimensionArray(int firstLength, int secondLength)
+{
+	int[,] array = new int[firstLength, secondLength];
+
+	for (int i = 0; i < array.GetLength(0); i++)
+		for (int j = 0; j < array.GetLength(1); j++)
+			array[i, j] = new Random().Next(0, 10);
+
+	return array;
+}
+
+void FillArray(int[,] array)
+{
+	for (int i = 0; i < array.GetLength(0); i++)
+		for (int j = 0; j < array.GetLength(1); j++)
+			array[i, j] = new Random().Next(0, 10);
+}
+
+
+void PrintArray(int[,] array)
+{
+	for (int i = 0; i < array.GetLength(0); i++)
+	{
+		for (int j = 0; j < array.GetLength(1); j++)
+			Console.Write($"{array[i, j]} ");
+		Console.WriteLine();
+	}
+}
